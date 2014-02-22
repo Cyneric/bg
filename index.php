ff26e7b47b23914369c52dd87fc94a49c2b9f001
cyneric <info@cyneric.de>
2014-02-22 12:23:06 +0100

    default html5 setup

---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>				
+<html lang="de">															
+<head>		
+	<meta charset="utf-8">
+	<title></title>	
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="js/jquery-1.10.2.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="shortcut icon" href="images/favicon.png" type="image/png" />
+	<link rel="icon" href="images/favicon.png" type="image/png" />
+</head>
+
+<body>
+	<div id="content">
+		<header>
+			<a href="index.php"><img src="images/logo.png" alt=""></a>
+		</header>
+		
+		<nav>
+		</nav>
+		
+		<footer>
+			<small>
+				Copyright &copy;
+				<time datetime="2013">2013</time>	
+			</small>
+		</footer>
+	</div>
+</body>
+</html>​
